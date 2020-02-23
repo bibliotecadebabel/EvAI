ec3206ec65967b991b043433352c048988f9d59e
FelixMorales <felixmorales@github.com>
2020-02-23 13:00:34 -0400

    testing

--- children/pytorch/Network.py
+++ children/pytorch/Network.py
@@ -229,11 +229,13 @@ class Network(nn.Module):
             i=0
             while i < p:
 
+                '''
                 if i == 1:
-                    print("L=", self.total_value, " i=", str(i)) 
+                    print("L=", self.total_value, " i=", str(i), "adn=", self.nodes[1].objects[0].adn," - ", self.nodes[2].objects[0].adn) 
 
                 if i % 200 == 199:
                     print("L=", self.total_value, " i=", str(i))
+                '''
                 self.assignLabels(labels)
                 #self.Reset_der_total()
                 self.total_value = 0
--- testNetworkPytorch.py
+++ testNetworkPytorch.py
@@ -218,21 +218,21 @@ def Test_Mutacion(dataGen):
     
     networkADN = ((0, 3, 10, 3, 3), (0, 10, 20, 9, 9), (1, 20, 2), (2,))
     mutationADN = ((0, 3, 10, 2, 2), (0, 10, 20, 10, 10), (1, 20, 2), (2,))
-    network = nw.Network(networkADN, cudaFlag=True)
+    network = nw.Network(networkADN, cudaFlag=False)
 
     for _,a in enumerate(batch):
         print("red original (network) k=", *network.adn)
-        network.Training(data=a[0], p=2000, dt=0.01, labels=a[1])
+        network.Training(data=a[0], p=600, dt=0.01, labels=a[1])
         print("mutando")
         network2 = MutateNetwork.executeMutation(network, mutationADN)
         print("entrando red mutada (network2): ", *network2.adn)
-        network2.Training(data=a[0],p=2000, dt=0.01, labels=a[1])
+        network2.Training(data=a[0],p=600, dt=0.01, labels=a[1])
         print("entrenando de nuevo red original (network)")
-        network.Training(data=a[0], p=2000, dt=0.01, labels=a[1])
+        network.Training(data=a[0], p=600, dt=0.01, labels=a[1])
         Inter.trakPytorch(network, "pokemon-netmap", dataGen)
 
 
-dataGen = GeneratorFromImage.GeneratorFromImage(2, 100, cuda=True)
+dataGen = GeneratorFromImage.GeneratorFromImage(2, 100, cuda=False)
 dataGen.dataConv2d()
 size = dataGen.size
 
