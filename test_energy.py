735c79d44bf23168549565d9efe6151c15ff8bbf
FelixMorales <felixmorales@github.com>
2020-04-07 17:35:48 -0400

    test_energy

--- test_energy.py
+++ test_energy.py
@@ -78,7 +78,7 @@ def Test_pytorch_energy():
 
     
     networkADN = ((0, 3, 3, x, y), (1, 3, 2), (2,))
-    mutationADN = ((0, 3, 3, x-1, y-1), (0, 6, 3, x, y), (1, 3, 2), (2,))
+    mutationADN = ((0, 3, 3, x-1, y-1), (0, 6, 3, x, y),(1, 3, 2), (2,))
     #mutationADN = ((0, 3, 4, x, y), (1, 4, 2), (2,))
     network = nw_p.Network(networkADN, cudaFlag=True)
     clone = MutateNetwork.executeMutation(network, mutationADN)
@@ -90,13 +90,15 @@ def Test_pytorch_energy():
     for _,a in enumerate(batch):
 
         while stop == False:
+            #print("entrenando red #1")
             network.Training(data=a[0], p=10, dt=0.01, labels=a[1])
+            #print("entrenando red #2")
             clone.Training(data=a[0], p=10, dt=0.01, labels=a[1])
             energy_2_filters = network.total_value
             energy_3_filters = clone.total_value
             i += 1
 
-            if energy_3_filters < energy_2_filters and i > 5:
+            if energy_3_filters < energy_2_filters:
                 stop = True
 
             if i % 10 == 0:
