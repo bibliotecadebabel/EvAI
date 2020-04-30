93137be98a98fdf8cd5004d6fe3746f65c7676c9
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-04-30 12:03:30 -0400

    fixed product_i

--- TestNetwork/commands/CommandExperimentMutation.py
+++ TestNetwork/commands/CommandExperimentMutation.py
@@ -14,9 +14,9 @@ class CommandExperimentMutation():
         self.__testName = testName
         self.__testDao = TestDAO.TestDAO()
         self.__testResultDao = TestResultDAO.TestResultDAO()
-        
+
     def __generateNetworks(self):
-        
+
         self.__networks = []
         self.__nodes = []
 
@@ -34,7 +34,7 @@ class CommandExperimentMutation():
         else:
             print("new center (cloning network)=", self.__bestNetwork.adn)
             centerNetwork = self.__bestNetwork.clone()
-        
+
         self.__nodes.append(nodeCenter)
         self.__networks.append(centerNetwork)
 
@@ -45,20 +45,20 @@ class CommandExperimentMutation():
             self.__networks.append(kidNetwork)
 
     def __saveEnergy(self):
-        
+
         for network in self.__networks:
 
             for node in self.__nodes:
-                
+
                 nodeAdn = self.__space.node2key(node)
 
                 if str(nodeAdn) == str(network.adn):
                     node.objects[0].objects[0].energy = network.total_value
-    
+
     def __getEnergyNode(self, node):
 
         return node.objects[0].objects[0].energy
-    
+
     def __getNodeCenter(self):
         space = self.__space
 
@@ -69,15 +69,15 @@ class CommandExperimentMutation():
 
             if str(nodeAdn) == str(space.center):
                 nodeCenter = node
-        
+
         return nodeCenter
 
 
     def execute(self, periodSave, periodNewSpace, totalIterations, dt):
-        
+
         dataGen = self.__dataGen
 
-        test_id = self.__testDao.insert(testName=self.__testName, periodSave=periodSave, dt=dt, 
+        test_id = self.__testDao.insert(testName=self.__testName, periodSave=periodSave, dt=dt,
                                             total=totalIterations, periodCenter=periodNewSpace)
 
 
@@ -87,19 +87,19 @@ class CommandExperimentMutation():
 
             for network in self.__networks:
                 network.Training(data=dataGen.data[0], labels=dataGen.data[1], dt=dt, p=1)
-            
+
             if j % periodSave == 0:
                 print("saving Energy, j=", j)
                 self.__saveEnergy()
                 self.__testResultDao.insert(idTest=test_id, iteration=j, dna_graph=self.__space)
-        
+
             if j % periodNewSpace == 0:
 
                 if self.__defineNewCenter() == True:
                     self.__generateNewSpace()
                     self.__generateNetworks()
 
-    
+
     def __getBestNetwork(self):
         lowestEnergy = 10000
         bestNetwork = None
@@ -110,36 +110,30 @@ class CommandExperimentMutation():
                 lowestEnergy = network.total_value
 
         return bestNetwork
-    
+
     def __defineNewCenter(self):
 
         value = False
         newBestNetwork = self.__getBestNetwork()
 
         if self.__bestNetwork is None:
-            
+
             value = True
             self.__bestNetwork = newBestNetwork
-        
+
         else:
 
             if str(newBestNetwork.adn) != str(self.__bestNetwork.adn):
                 value = True
                 self.__bestNetwork = newBestNetwork
-        
+
         return value
 
     def __generateNewSpace(self):
         oldSpace = self.__space
 
-        newSpace = DNA_Graph(center=self.__bestNetwork.adn, size=oldSpace.size, dim=(oldSpace.x_dim, oldSpace.y_dim), 
+        newSpace = DNA_Graph(center=self.__bestNetwork.adn, size=oldSpace.size, dim=(oldSpace.x_dim, oldSpace.y_dim),
                                 condition=oldSpace.condition, typos=oldSpace.typos, type_add_layer=oldSpace.version)
-        
+
         self.__space = None
         self.__space = newSpace
-
-
-
-
-            
-    
