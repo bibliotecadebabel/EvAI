d32767a35eead7ba3fb0545c1401828eae128179
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-05-16 23:54:44 -0400

    increase sleep time7

--- Product_f_cifar.py
+++ Product_f_cifar.py
@@ -120,7 +120,7 @@ class Status():
             stream=status.stream
             net=stream.get_net(center)
             net.generateEnergy(status.Data_gen)
-            print(f'The acurrarcy is : {net.get.Acurracy()}')
+            print(f'The acurrarcy is : {net.getAcurracy()}')
             time.sleep(100)
         pass
 
