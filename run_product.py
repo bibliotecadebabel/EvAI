da50531a8b80b601c22ceeae9cc171ebc1917b47
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-05-17 22:15:26 -0400

    changed batch size

--- run_product.py
+++ run_product.py
@@ -1,6 +1,6 @@
 import Experiments.product_experiments  as experiements
 
 
-#run_slow_ncf()
-experiments.run_cifar_user_input()
+#experiements.run_slow_ncf()
+experiements.run_cifar_user_input()
 #import_test()
