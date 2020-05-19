30fef73a17c62f26ea2ae2724bd0b1d15cc8f13a
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-05-19 00:06:03 -0400

    fixed bug in produdct experiment

--- run_product.py
+++ run_product.py
@@ -1,11 +1,11 @@
 import Experiments.product_experiments  as experiments
 
 
-#experiments.run_cifar_user_input_no_save()
+experiments.run_cifar_user_input_no_save()
 #experiments.run_cifar_user_input_bidi_save()
 #experiments.run_cifar_user_no_image()
 #experiments.run_local_no_image()
-experiments.run_local_ac()
+#experiments.run_local_ac()
 ##experiments.run_cifar_user_input_bidi()
 #experiments.run_cifar_user_input_bidi_no_save()
 #experiments.run_slow_ncf()
