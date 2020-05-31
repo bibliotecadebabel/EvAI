48bf1b15b978ce1d8088656576298423ce41d5d0
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-05-31 11:29:12 -0400

    created uniform selector

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -4,7 +4,7 @@ from DNA_conditions import max_layer,max_filter,max_filter_dense
 from DNA_creators import Creator
 from DNA_Graph import DNA_Graph
 from DNA_creators import Creator_from_selection as Creator_s
-from utilities.Abstract_classes.classes.positive_random_selector import centered_random_selector as random_selector
+from utilities.Abstract_classes.classes.uniform_random_selector import centered_random_selector as random_selector
 import TestNetwork.ExperimentSettings as ExperimentSettings
 import numpy as np
 ###### EXPERIMENT SETTINGS ######
