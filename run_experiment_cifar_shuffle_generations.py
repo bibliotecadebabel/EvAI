bce8650d364b622129670439ee69b774af20602e
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-05-31 11:08:57 -0400

    removed add_pool mutation

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -39,7 +39,7 @@ def DNA_Creator_s(x,y, dna):
         directions=version, num_actions=num_actions,
         mutations=(
         (0,1,0,0),(0,-1,0,0),
-        (1,0,0,0),(4,0,0,0),
+        (1,0,0,0),
         (0,0,1,1),(0,0,-1,-1),
         (0,0,1),(0,0,-1),
         ))
