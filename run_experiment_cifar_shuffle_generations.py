3b2d94a7e59e78e99423d9caa3d28bdab15f2022
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-06-02 21:32:58 -0400

    put add filters back

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -49,6 +49,7 @@ def DNA_Creator_s(x,y, dna, version):
         (4,0,0,0),
         (1,0,0,0),
         (0,0,1),
+        (0,1,0,0),
         ))
     selector.update(dna)
     actions=selector.get_predicted_actions()
