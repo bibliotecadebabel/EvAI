f9047751af8804a182817a1d8ece1d6a1b7d3cdd
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-06-05 01:10:35 -0400

    selected undesired mutations

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -48,9 +48,8 @@ def DNA_Creator_s(x,y, dna, version):
     selector=random_selector(condition=condition,
         directions=version, num_actions=num_actions,
         mutations=(
-        (1,0,0,0),(1,0,0,0),
+        (1,0,0,0),
         (0,0,1),(0,0,-1),
-        (0,1,0,0),(0,-1,0,0),
         (0,0,1,1),(0,0,-1,-1),
         ))
     selector.update(dna)
