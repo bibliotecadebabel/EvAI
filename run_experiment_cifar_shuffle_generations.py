16b9b57f68fed36241721a9d9c6d2bb8b65457ab
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-06-07 10:09:39 -0400

    reducing conv layers

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -11,7 +11,7 @@ import const.versions as directions_version
 import numpy as np
 import test_DNAs as DNAs
 ###### EXPERIMENT SETTINGS ######
-
+"""
 def dropout_function(base_p, total_conv2d, index_conv2d):
 
     value = base_p / (total_conv2d - index_conv2d)+base_p/2
@@ -24,7 +24,7 @@ def dropout_function(base_p, total_conv2d, index_conv2d):
     value = base_p +(3/5*base_p-base_p)*(total_conv2d - index_conv2d)/total_conv2d
     #print("conv2d: ", index_conv2d, " - dropout: ", value)
     return value
-"""
+
 
 
 
