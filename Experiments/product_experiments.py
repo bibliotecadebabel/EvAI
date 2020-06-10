159f90c07739934cbe4d992d6b0f1d0ce1ef54dc
bibliotecadebabel <bibliotecadebabel6616@gmail.com>
2020-06-10 11:43:07 -0400

    testing run_product

--- Experiments/product_experiments.py
+++ Experiments/product_experiments.py
@@ -45,6 +45,45 @@ def run_cifar_user_input_bidi(save = False):
     status.log_size=int(input("Log size : "))
     status.min_log_size=100
     status.S=int(input("Batch size : "))
+    status.cuda=True
+    status.mutation_coefficient=float(input("mutation_coefficient : "))
+    if save:
+        status.experiment_name=input("insert experiment name : ")
+        status.save_space_period=int(input("save_space_period : "))
+        status.save_net_period=int(input("save_space_net_period : "))
+    status.save2database=save
+    x=32
+    y=32
+    status.Center=((-1,1,3,x,y),
+            (0,3, 15, 3 , 3),
+            (0,18, 15, 3,  3),
+            (0,33, 50, x, y),
+            (1, 50,10),
+             (2,),
+            (3,-1,0),
+            (3,0,1),(3,-1,1),
+            (3,1,2),(3,0,2),(3,-1,2),
+            (3,2,3),
+            (3,3,4))
+    program.run(status)
+
+"""
+def run_cifar_user_input_bidi(save = False):
+    import Product_f_cifar_save_2 as program
+    status=program.Status()
+    status.dt_Max=0.01
+    status.dt_min=0.0001
+    status.clear_period=200000
+    status.max_iter=20001
+    status.restart_period=200
+    status.max_layer=8
+    status.max_filter=51
+    from utilities.Abstract_classes.classes.centered_random_selector_bidi import(
+        centered_random_selector as Selector)
+    status.Selector_creator=Selector
+    status.log_size=int(input("Log size : "))
+    status.min_log_size=100
+    status.S=int(input("Batch size : "))
     status.cuda=False
     status.mutation_coefficient=float(input("mutation_coefficient : "))
     if save:
@@ -66,6 +105,7 @@ def run_cifar_user_input_bidi(save = False):
             (3,2,3),
             (3,3,4))
     program.run(status)
+"""
 
 def run_cifar_user_input_bidi_back_up(save = False):
     import Product_f_cifar_save_2 as program
