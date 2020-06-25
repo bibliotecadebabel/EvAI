5d897f89f603b1759c68298e1cca4e6e051482cc
FelixMorales <felixmorales@github.com>
2020-06-24 22:04:09 -0400

    fix

--- Dyamic_DNA_f_methods.py
+++ Dyamic_DNA_f_methods.py
@@ -186,11 +186,11 @@ def node_max_particles(phase_space):
     max_index=np.argmax(np.array(particles))
     return phase_space.objects[max_index]
 
-def node_2nd_max_particles(phase_space, max_particles):
+def node_2nd_max_particles(phase_space, node_max):
     particles=[
         phase_space.node2particles(node) for node in
             phase_space.objects
-                if phase_space.node2particles(node) != max_particles
+                if node != node_max
             ]
     max_index=np.argmax(np.array(particles))
     return phase_space.objects[max_index]
@@ -321,8 +321,8 @@ def update_from_select_09(self):
     node_max=node_max_particles(phase_space)
     p_m=Funct.node2num_particles(node_max)
     node_c = phase_space.key2node(phase_space.DNA_graph.center)
-    p_c=Funct.node2num_particles(node_c)
-    node_max_2 = node_2nd_max_particles(phase_space, p_m)
+    #p_c=Funct.node2num_particles(node_c)
+    node_max_2 = node_2nd_max_particles(phase_space, node_max)
     p_m_2 = Funct.node2num_particles(node_max_2)
     print(f'The value of p_m is : {p_m} and p_m_2 is : {p_m_2} ')
     if (p_m>p_m_2*3 and node_max != node_c):
