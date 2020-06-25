85ca801489b513af2950e3852bcd4137f79a51b5
FelixMorales <felixmorales@github.com>
2020-06-24 22:15:21 -0400

    testing

--- Dyamic_DNA_f_methods.py
+++ Dyamic_DNA_f_methods.py
@@ -322,9 +322,14 @@ def update_from_select_09(self):
     p_m=Funct.node2num_particles(node_max)
     node_c = phase_space.key2node(phase_space.DNA_graph.center)
     #p_c=Funct.node2num_particles(node_c)
-    
     node_max_2 = node_2nd_max_particles(phase_space, node_max)
     p_m_2 = Funct.node2num_particles(node_max_2)
+    list_particles = [
+        phase_space.node2particles(node) for node in
+            phase_space.objects
+    ]
+    print("particles: ", list_particles)
+    time.sleep(10)
     print(f'The value of p_m is : {p_m} and p_m_2 is : {p_m_2} ')
     if (p_m>p_m_2*3 and node_max != node_c):
         phase_space.time=0
