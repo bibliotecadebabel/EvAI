5e401dc8e0314b98ffa502931591495ddf441f43
FelixMorales <felixmorales@github.com>
2020-06-26 12:14:09 -0400

    revert changes

--- run_post_training.py
+++ run_post_training.py
@@ -15,8 +15,6 @@ import os
 import utilities.NetworkStorage as NetworkStorage
 import children.pytorch.MutationManager as MutationManager
 import utilities.MemoryManager as MemoryManager
-import torch
-import gc
 
 ###### EXPERIMENT SETTINGS ######
 
@@ -174,32 +172,15 @@ if __name__ == '__main__':
         fileManager.setFileName("post_training_"+selected_test.name)
         fileManager.writeFile("")
 
-    #path = os.path.join("saved_models","product_database", "13_post-training-model_alai35753_0")
-    #network = NetworkStorage.loadNetwork(fileName=None, settings=settings, path=path)
+    path = os.path.join("saved_models","product_database", "13_recovering_2ndorder_eval_model_1163")
+    network = NetworkStorage.loadNetwork(fileName=None, settings=settings, path=path)
 
-    INIT_DNA_TESTING = ((-1, 1, 3, 32, 32), (0, 3, 128, 3, 3), (0, 128, 128, 3, 3), (0, 128, 128, 4, 4), 
-                (0, 128, 128, 3, 3), (0, 128, 256, 5, 5, 2), (0, 384, 128, 3, 3, 2), (0, 128, 128, 3, 3), 
-                (0, 128, 512, 3, 3, 2), (0, 128, 128, 2, 2), (0, 256, 256, 3, 3), (0, 256, 256, 5, 5), 
-                (0, 512, 256, 3, 3), (0, 256, 256, 3, 3), (0, 768, 512, 4, 4, 2), (0, 512, 512, 4, 4), 
-                (0, 512, 512, 2, 2), (0, 512, 512, 3, 3), (0, 1024, 512, 2, 2), (0, 512, 256, 8, 8), 
-                (1, 256, 10), (2,), (3, -1, 0), (3, 0, 1), (3, 1, 2), (3, 2, 3), (3, 3, 4), (3, 0, 5), (3, 4, 5), 
-                (3, 5, 6), (3, 6, 7), (3, 5, 8), (3, 4, 9), (3, 8, 10), (3, 9, 10), (3, 10, 11), (3, 4, 11), 
-                (3, 11, 12), (3, 12, 13), (3, 7, 13), (3, 7, 14), (3, 14, 15), (3, 15, 16), (3, 13, 17), 
-                (3, 16, 17), (3, 17, 18), (3, 18, 19), (3, 19, 20))
-
-    network = nw.Network(adn=INIT_DNA_TESTING, cudaFlag=settings.cuda,
-                                    momentum=settings.momentum, weight_decay=settings.weight_decay,
-                                    enable_activation=settings.enable_activation,
-                                    enable_track_stats=settings.enable_track_stats, dropout_value=settings.dropout_value,
-                                    dropout_function=settings.dropout_function, enable_last_activation=settings.enable_last_activation,
-                                    version=settings.version, eps_batchnorm=settings.eps_batchorm)
     network.generateEnergy(dataGen)
     print("Loaded acc: ", network.getAcurracy())
 
-    #mutationManager = MutationManager.MutationManager(directions_version="convex")
-    #memoryManager = MemoryManager.MemoryManager()
+    mutationManager = MutationManager.MutationManager(directions_version="convex")
+    memoryManager = MemoryManager.MemoryManager()
 
-    '''
     NEW_DNA = ((-1, 1, 3, 32, 32), (0, 3, 128, 3, 3), (0, 128, 128, 3, 3), (0, 128, 128, 4, 4), 
                 (0, 128, 128, 3, 3), (0, 128, 256, 5, 5, 2), (0, 384, 128, 3, 3, 2), (0, 128, 128, 3, 3), 
                 (0, 128, 512, 3, 3, 2), (0, 128, 128, 2, 2), (0, 256, 256, 3, 3), (0, 256, 256, 5, 5), 
@@ -212,16 +193,12 @@ if __name__ == '__main__':
 
     oldNetwork = network
     network = mutationManager.executeMutation(network, NEW_DNA)
-    '''
     avg_factor = len(settings.init_dt_array) // 4
 
-    #memoryManager.deleteNetwork(oldNetwork)
-    
-    gc.collect()
-    torch.cuda.empty_cache()
+    memoryManager.deleteNetwork(oldNetwork)
 
-    #network.generateEnergy(settings.dataGen)
-    #saveModel(test=selected_test, network=network, iteration=0)
+    network.generateEnergy(settings.dataGen)
+    saveModel(test=selected_test, network=network, iteration=0)
 
     for i in range(settings.max_init_iter):
         print("iteration: ", i+1)
