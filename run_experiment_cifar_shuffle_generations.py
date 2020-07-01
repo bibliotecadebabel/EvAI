8b892a569ae8c17895ad3af5123cb7ec0b5e2842
FelixMorales <felixmorales@github.com>
2020-07-01 10:50:33 -0400

    testing

--- run_experiment_cifar_shuffle_generations.py
+++ run_experiment_cifar_shuffle_generations.py
@@ -39,11 +39,14 @@ def dropout_function(base_p, total_conv2d, index_conv2d, isPool=False):
 def dropout_function(base_p, total_layers, index_layer, isPool=False):
 
     value = 0
-    if index_layer != 0 and isPool == False:
+    if isPool == False and index_layer % 2 == 0:
         value = base_p
 
     if index_layer == total_layers - 2:
         value = base_p
+    
+    if index_layer == 0:
+        value = 0
 
     print("conv2d: ", index_layer, " - dropout: ", value)
 
